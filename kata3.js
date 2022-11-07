bb71e373aeffef5c2c846be1a5fcd90f92e01b45
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-07 10:00:22 -0700

    submit kata3 to loop through a string and count the number of vowels. Also add .toLowerCase() to account for upper case vowels that might appear

---
+++ kata3.js
@@ -0,0 +1,16 @@
+const numberOfVowels = function(data) {
+  // loop through string and for every vowel found add 1 to vowels variable
+  let vowels = 0;
+  for (i = 0; i < data.length; i++) {
+    data = data.toLowerCase(); // assignment unclear as to whether we should account for upper case vowels as well so decided to go for it and add counting for that also
+    if (data[i] == "a" || data[i] == "e" || data[i] == "i" || data[i] == "o" || data[i] == "u") {
+      vowels++;
+    }
+  };
+  return vowels;
+}
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
+console.log(numberOfVowels("AEIOU"));
