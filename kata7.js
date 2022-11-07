5065da82123ee6211b06378f97760b8dedbcb45f
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-07 13:38:16 -0700

    submit kata7 with condition and loop

---
+++ kata7.js
@@ -0,0 +1,39 @@
+const checkAir = function (samples, threshold) {
+  // Code here!
+  // count cleans
+  let dirty = 0;
+  let total = 0;
+  for (let i = 0; i < samples.length; i++) {
+    if (samples[i] === "dirty") {
+      dirty++; // count dirties
+    }
+    total++; // count total number
+  }
+  const ratio = dirty / total; // dirty / total = ratio (as decimal)
+
+  if (ratio >= threshold) { // if ratio >= threshold then return "Polluted" else return "Clean"
+    return "Polluted";
+  } else {
+    return "Clean";
+  }
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+));
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.4
+));
