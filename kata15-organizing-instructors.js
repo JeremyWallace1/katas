83cfe41ab82702e6739efe62979c70c9d7378fbc
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-20 19:39:17 -0700

    fixed spacing and indentation in kata15

--- kata15-organizing-instructors.js
+++ kata15-organizing-instructors.js
@@ -3,26 +3,26 @@ const organizeInstructors = function(instructors) {
   // read course value for each object in instructors
   let organized = {};
   for (let i = 0; i < instructors.length; i++) {
-    if (instructors[i].course === "Blockchain") { 
+    if (instructors[i].course === "Blockchain") {
       // if instructors[i].course is Blockchain, Blockchain.push(instructors.name)
       if (organized.Blockchain) {
         organized.Blockchain.push(instructors[i].name);
       } else {
         organized.Blockchain = [instructors[i].name];
       }
-    } else if (instructors[i].course === "iOS") { 
+    } else if (instructors[i].course === "iOS") {
       // if instructors[i]course is iOS, iOS.push(instructors.name)
       if (organized.iOS) {
-          organized.iOS.push(instructors[i].name);
+        organized.iOS.push(instructors[i].name);
       } else {
-          organized.iOS = [instructors[i].name];
+        organized.iOS = [instructors[i].name];
       }
-    } else if (instructors[i].course === "Web") { 
+    } else if (instructors[i].course === "Web") {
       // if instructors[i]course is Web, Web.push(instructors.name)
       if (organized.Web) {
         organized.Web.push(instructors[i].name);
       } else {
-          organized.Web = [instructors[i].name];
+        organized.Web = [instructors[i].name];
       }
     }
   }
