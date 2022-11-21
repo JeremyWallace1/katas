0f356846c65be5b758f7429d2b5929610f517c40
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-20 18:55:00 -0700

    ran eslint, also change c-style loop to for..in loop, replaced == with ===

--- kata3.js
+++ kata3.js
@@ -1,14 +1,14 @@
 const numberOfVowels = function(data) {
   // loop through string and for every vowel found add 1 to vowels variable
   let vowels = 0;
-  for (let i = 0; i < data.length; i++) {
+  for (let i in data) {
     data = data.toLowerCase(); // assignment unclear as to whether we should account for upper case vowels as well so decided to go for it and add counting for that also
-    if (data[i] == "a" || data[i] == "e" || data[i] == "i" || data[i] == "o" || data[i] == "u") {
+    if (data[i] === "a" || data[i] === "e" || data[i] === "i" || data[i] === "o" || data[i] === "u") {
       vowels++;
     }
-  };
+  }
   return vowels;
-}
+};
 
 console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
